675150c7c07b65e4674723e9567f1c1d5c237f76
Simrun Basuita <hello@sim.run>
2017-10-22 21:13:03 +0100

    Lesson 5: 'Applying AI to Sudoku'

--- solution.py
+++ solution.py
@@ -27,9 +27,21 @@ def naked_twins(values):
     # Find all instances of naked twins
     # Eliminate the naked twins as possibilities for their peers
 
+rows = 'ABCDEFGHI'
+cols = '123456789'
+
 def cross(A, B):
     "Cross product of elements in A and elements in B."
-    pass
+    return [s+t for s in A for t in B]
+
+boxes = cross(rows, cols)
+
+row_units = [cross(r, cols) for r in rows]
+column_units = [cross(rows, c) for c in cols]
+square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
+unitlist = row_units + column_units + square_units
+units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
+peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
 
 def grid_values(grid):
     """
@@ -41,7 +53,12 @@ def grid_values(grid):
             Keys: The boxes, e.g., 'A1'
             Values: The value in each box, e.g., '8'. If the box has no value, then the value will be '123456789'.
     """
-    pass
+    d = {}
+    for i in range(81):
+        d[boxes[i]] = grid[i]
+        if d[boxes[i]] == '.': d[boxes[i]] = '123456789'
+
+    return d
 
 def display(values):
     """
@@ -49,19 +66,66 @@ def display(values):
     Args:
         values(dict): The sudoku in dictionary form
     """
-    pass
+    width = 1 + max(len(values[s]) for s in boxes)
+    line = '+'.join(['-' * (width * 3)] * 3)
+    for r in rows:
+        print(''.join(values[r + c].center(width) + ('|' if c in '36' else '')
+                      for c in cols))
+        if r in 'CF': print(line)
+    return
 
 def eliminate(values):
-    pass
+    for box, value in values.items():
+        if len(value) != 1: continue
+
+        for peer in peers[box]:
+            values[peer] = values[peer].replace(value, '')
+
+    return values
 
 def only_choice(values):
-    pass
+    for unit in unitlist:
+        for i in '123456789':
+            iboxes = [box for box in unit if i in values[box]]
+            if len(iboxes) == 1: values[iboxes[0]] = i
+
+    return values
 
 def reduce_puzzle(values):
-    pass
+    stalled = False
+    while not stalled:
+        # Check how many boxes have a determined value
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+
+        # Your code here: Use the Eliminate Strategy
+        values = eliminate(values)
+        # Your code here: Use the Only Choice Strategy
+        values = only_choice(values)
+        # Check how many boxes have a determined value, to compare
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        # If no new values were added, stop the loop.
+        stalled = solved_values_before == solved_values_after
+        # Sanity check, return False if there is a box with zero available values:
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
 
 def search(values):
-    pass
+    values = reduce_puzzle(values)
+    if values == False: return False
+    # if all(len(values[box]) == 1 for box in boxes): return values
+    # Choose one of the unfilled squares with the fewest possibilities
+    for i in range(2,10):
+        unfilled = [box for box in boxes if len(values[box]) == i]
+        if unfilled: break
+        if i == 9: return values
+    searchbox = unfilled[0]
+    # Now use recursion to solve each one of the resulting sudokus, and if one returns a value (not False), return that answer!
+    for v in values[searchbox]:
+        newvalues = values.copy()
+        newvalues[searchbox] = v
+        newvalues = search(newvalues)
+        if newvalues: return newvalues
 
 def solve(grid):
     """
@@ -72,6 +136,7 @@ def solve(grid):
     Returns:
         The dictionary representation of the final sudoku grid. False if no solution exists.
     """
+    return search(grid_values(grid))
 
 if __name__ == '__main__':
     diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
